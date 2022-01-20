8d00118758e05e3c583464884cb4dc40f0574a80
phoo <ayemyatmyatkyaw5455499@gmail.com>
2022-01-20 23:50:26 +0630

    nav-responsive

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let btn4 = document.getElementById('shelf');
 let btn5 = document.getElementById('basket');
 let btn6 = document.getElementById('gift');
 
-btn1.onclick = function abc(){
+btn1.onclick = function abc() {
 	gallery1.style.display = 'grid';
 	gallery2.style.display = 'none';
 	gallery3.style.display = 'none';
@@ -14,7 +14,7 @@ btn1.onclick = function abc(){
 	gallery6.style.display = 'none';
 }
 
-btn2.onclick = function abc(){
+btn2.onclick = function abc() {
 	gallery1.style.display = 'none';
 	gallery2.style.display = 'grid';
 	gallery3.style.display = 'none';
@@ -23,7 +23,7 @@ btn2.onclick = function abc(){
 	gallery6.style.display = 'none';
 }
 
-btn3.onclick = function abc(){
+btn3.onclick = function abc() {
 	gallery1.style.display = 'none';
 	gallery2.style.display = 'none';
 	gallery3.style.display = 'grid';
@@ -32,7 +32,7 @@ btn3.onclick = function abc(){
 	gallery6.style.display = 'none';
 }
 
-btn4.onclick = function abc(){
+btn4.onclick = function abc() {
 	gallery1.style.display = 'none';
 	gallery2.style.display = 'none';
 	gallery3.style.display = 'none';
@@ -41,7 +41,7 @@ btn4.onclick = function abc(){
 	gallery6.style.display = 'none';
 }
 
-btn5.onclick = function abc(){
+btn5.onclick = function abc() {
 	gallery1.style.display = 'none';
 	gallery2.style.display = 'none';
 	gallery3.style.display = 'none';
@@ -50,11 +50,19 @@ btn5.onclick = function abc(){
 	gallery6.style.display = 'none';
 }
 
-btn6.onclick = function abc(){
+btn6.onclick = function abc() {
 	gallery1.style.display = 'block';
 	gallery2.style.display = 'none';
 	gallery3.style.display = 'none';
 	gallery4.style.display = 'none';
 	gallery5.style.display = 'none';
 	gallery6.style.display = 'grid';
+}
+
+let nav_show = document.getElementsByClassName("nav_show")[0];
+let nav_class = document.getElementsByClassName("nav_ul")[0];
+
+nav_show.onclick = function nav() {
+	nav_class.classList.toggle('nav_hide');
+	nav_show.classList.toggle('nav_color');
 }
